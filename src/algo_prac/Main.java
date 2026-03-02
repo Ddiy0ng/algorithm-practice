a57319069bea7f4d64efbe318c2464cf92ef33fa
Ddiy0ng <bueong0627@gmail.com>
2026-03-02 16:50:12 +0900

    3번 문제

--- src/algo_prac/Main.java
+++ src/algo_prac/Main.java
@@ -1,14 +1,34 @@
 package algo_prac;
 
-import algo_prac.Problems;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Problems problems = new Problems();
-		System.out.println(problems.problem002());
+		
+		final Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();	// 수 개수
+		int count = sc.nextInt(); // 질의 개수
+		
+		int[] arr = new int[N];
+		arr[0] = sc.nextInt();
+		for (int i = 1; i < N; i++)
+			arr[i] = arr[i - 1] + sc.nextInt();
+		
+		int[] results = new int[count];
+		for (int k = 0; k < count; k++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			
+			if(i == 1)
+				results[k] = arr[j - 1];
+			else
+				results[k] = arr[j - 1] - arr[i - 2];
+		}
+		
+		for(int i = 0; i < count; i++)
+			System.out.println(results[i]);
 	}
 
 }
--- src/algo_prac/Problems.java
+++ src/algo_prac/Problems.java
@@ -7,7 +7,7 @@ public class Problems {
 	final Scanner sc = new Scanner(System.in);
 	
 	// 숫자 개수와, 공백으로 구분없는 숫자들을 입력받고 숫자 합 출력하기
-	protected int problem001() {
+	protected void problem001() {
 		System.out.print("1. enter the size: ");
 		int N = sc.nextInt();
 		
@@ -18,11 +18,11 @@ public class Problems {
 		for (int i = 0; i < N; i++)
 			sum += nums.charAt(i)-'0';	// '0' == 48, 0 == 0	ASCII
 		
-		return sum;
+		System.out.println(sum);
 	}
 	
 	// 과목 개수와 공백으로 구분된 성적들을 입력받고 맞춤형 계산법으로 평균 구해 출력하기
-	protected double problem002() {
+	protected void problem002() {
 		System.out.print("1. enter the lectures count: ");
 		int N = sc.nextInt();
 		
@@ -42,6 +42,31 @@ public class Problems {
 			sum += scores[i];
 		}
 
-		return sum / N;
+		System.out.println(sum / N);
+	}
+	
+	// 구간합 구하기1
+	protected void problem003() {
+		int N = sc.nextInt();	// 수 개수
+		int count = sc.nextInt(); // 질의 개수
+		
+		int[] arr = new int[N];
+		arr[0] = sc.nextInt();
+		for (int i = 1; i < N; i++)
+			arr[i] = arr[i - 1] + sc.nextInt();
+		
+		int[] results = new int[count];
+		for (int k = 0; k > count; k++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			
+			if( i == 0)
+				results[k] = arr[j];
+			else
+				results[k] = arr[j] - arr[i - 1];
+		}
+		
+		for(int i = 0; i < count; i++)
+			System.out.println(results[i]);
 	}
 }
